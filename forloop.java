d673b9800bb49ae0f0a64442eb2f6d7ffaab47e5
GHOUSE <grgr6566@gmail.com>
2023-02-16 07:50:29 +0530

    adding doc into the project

--- forloop.java
+++ forloop.java
@@ -2,7 +2,7 @@ public class forloop {
     public static void main(String[] args) {
         int i;
         for(i=0;i<=8;i++){
-            System.out.println(i);
+            System.out.println(" the result " +i);
         }
     }
 }
